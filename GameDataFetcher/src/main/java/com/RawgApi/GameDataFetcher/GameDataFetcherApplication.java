4a6ff8a7a212ed08cb36ae4088eff4f64783c199
ranndy <ranndy180@gmail.com>
2024-11-17 15:31:37 -0500

    primer commit

---
+++ GameDataFetcher/src/main/java/com/RawgApi/GameDataFetcher/GameDataFetcherApplication.java
@@ -0,0 +1,18 @@
+package com.RawgApi.GameDataFetcher;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataFetcherApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDataFetcherApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ GameDataFetcher/src/test/java/com/RawgApi/GameDataFetcher/GameDataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RawgApi.GameDataFetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDataFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
